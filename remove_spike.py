78d2a5930af03f3204a235b92dd6b556509987cf
yenchy0310 <yenchy0310@gmail.com>
2021-06-07 12:21:53 +0800

    remove_spike by ratio method

--- remove_spike.py
+++ remove_spike.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def remove_spike(data, channel, threshold):
-    AMS1_spike_index = data[abs(data[channel]-data[channel][0:10].mean())>threshold].index
-    AMS2_spike_index = data[abs(data[channel]-data[channel][0:10].mean())>threshold].index
+    AMS1_spike_index = data[abs(data[channel]-data[channel][0:10].mean())/data[channel][0:10].mean()>threshold].index
+    AMS2_spike_index = data[abs(data[channel]-data[channel][0:10].mean())/data[channel][0:10].mean()>threshold].index
     print('AMS1_spike_index', AMS1_spike_index)
     print('AMS2_spike_index', AMS2_spike_index)
     spike_index = np.append(AMS1_spike_index, AMS2_spike_index)
